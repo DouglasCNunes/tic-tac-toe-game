c75b5688ab52683f1bf68ceeb145aa2f618ff360
Douglas Nunes <douglasc7nunes@gmail.com>
2022-07-26 08:30:07 -0300

    Third commit
    Code can bee build.

--- src/tictactoe/Board.java
+++ src/tictactoe/Board.java
@@ -1,6 +1,7 @@
 package tictactoe;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Board {
@@ -9,44 +10,31 @@ public class Board {
     private static Integer[][] combination = {{1,2,3},{4,5,6},{7,8,9},{1,4,7},{2,5,8},{3,6,9},{1,5,9}};
 
     public Board() {
-        List player1 = new ArrayList<>();
-        List player2 = new ArrayList<>();
-    }
-
-    private boolean checkPosition(List<Integer> player, int position) {
-        for (int i = 0; i < player.size(); i++) {
-            if(position == player.get(i)) {
-                return false;
-            }
-        }
-        return true;
+        player1 = new ArrayList<>();
+        player2 = new ArrayList<>();
     }
 
     private boolean checkWin(List<Integer> player) {
         for (int i = 0; i < combination.length; i++) {
-            int check = 0;
-            for (int j = 0; j < 3; j++) {
-                if(player.contains(combination[i][j])) {
-                    check++;
-                }
-            }
-            if(check==3) return true;
+            if(player.containsAll(Arrays.asList(combination[i]))) return true;
         }
         return false;
     }
 
     public int play(int player, int position) {
         //Player1 = zero, Player 2 = positive number
-        if(checkPosition(player1, position)==false || checkPosition(player2, position)==false) return -1;
+        if(player1.contains(position) || player2.contains(position)) return -1; //Position has already been played
         if(player == 0) player1.add(position);
         else player2.add(position);
-        return 1;
+        return 1; //Play was performed;
     }
 
-    public void checkGame() {
-        //Win Player1 = 0, Win PLayer2 = 1, In game = 2, Drawn = 3
+    public int checkGame() {
+        //Win Player1 = 0, Win PLayer2 = 1, In game = 3, Drawn = 2
         if(checkWin(player1)==true) return 0;
         if(checkWin(player2)==true) return 1;
+        if((player1.size()+player2.size()==9)) return 3;
+        return 2;
     }
 
 }
--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -1,8 +1,21 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Board tictactoe = new Board();
-        System.out.println("Hey!");
+        Board game = new Board();
+        Scanner input = new Scanner(System.in);
+        int player = 0,position;
+        
+        while(game.checkGame()==2) {
+            if(player==0) System.out.println("It is time of player 1.");
+            else System.out.println("It is time of player 2.");
+            position = input.nextInt();
+            game.play(player, position);
+            if(player==0) player++;
+            else player--;
+            if(game.checkGame()==1) System.out.println("Sucesso");
+        }
     }
 }
